7b31a1a740056241123c08a217802be913539420
Your Name <you@example.com>
2022-08-14 14:12:33 +0300

    feat: add react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Header from "./Components/Header/Header";
 import SimpleForm from "./Pages/SimpleForm/SimpleForm";
+import CreditCardForm from "./Pages/CreditCardForm/CreditCardForm";
 
 function App() {
   return (
     <div className="container mx-auto">
-      <Header/>
-      <SimpleForm/>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<SimpleForm/>}/>
+          <Route path="/credit-card" element={<CreditCardForm/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,17 +1,20 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
 export default function Header() {
   return (
     <div className="container mx-auto flex justify-between items-center py-4">
-      <div>
+      <nav>
         <ul className="flex flex-row">
           <li className="cursor-pointer pr-1 text-blue-900">
-            Simple
+            <Link to="/">Simple</Link>
           </li>
           |
           <li className="cursor-pointer px-1 text-blue-900">
-            CreditCard
+            <Link to="/credit-card">CreditCard</Link>
           </li>
         </ul>
-      </div>
+      </nav>
       <div>Forms</div>
     </div>
   )
